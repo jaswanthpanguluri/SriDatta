8f377372d2994f9de786d39196ec78bc8db26de5
jaswanth <pangulurijaswanth9@gmail.com>
2024-08-08 09:27:41 +0530

    homeImageChanges

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,65 +8,65 @@ import { CurdService } from 'src/app/services/curd.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls:['./home.component.scss']
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
-  banners:any;
-  iconicBanners:any;
-  smallBanners:any;
+export class HomeComponent implements OnInit {
+  banners: any;
+  iconicBanners: any;
+  smallBanners: any;
   productSecions: any;
-  city:any;
-  countryname:any;
-  currency:any;
-   showLines: number = 3;
+  city: any;
+  countryname: any;
+  currency: any;
+  showLines: number = 3;
+  promotionBanner: any;
+  shopByGender: any;
 
   showMore() {
     this.showLines += 3; // Increase by desired number of lines
   }
-  constructor(private _crud:CurdService, private route:Router,  private renderer: Renderer2,  private meta:Meta, private titleService:Title)
-  {
-  
+  constructor(private _crud: CurdService, private route: Router, private renderer: Renderer2, private meta: Meta, private titleService: Title) {
+
   }
 
-  addLoader()
-  {
+  addLoader() {
     this.renderer.addClass(document.body, 'bodyloader');
   }
-  removeLoader()
-  {
+  removeLoader() {
     this.renderer.removeClass(document.body, 'bodyloader');
   }
 
   ngOnInit(): void {
     this.titleService.setTitle('Order Cake Online | Send Cake Online to India - Country Oven');
     this.meta.updateTag({ name: 'description', content: 'Order cake online at Countryoven. Send a cake to India with same-day delivery from anywhere with one click.Tap to Order now! and surprise your loved ones.' });
-    this.meta.updateTag({ name: 'keywords',  content: 'Order Birthday Cake Online ,order cake online ,birthday cakes delivered ,send cake to India ,online cake delivery in india ,send birthday cake online' });
-    this.meta.updateTag({ name: 'classification',  content: 'Order Birthday Cake Online ,order cake online ,birthday cakes delivered ,send cake to India ,online cake delivery in india ,send birthday cake online' });
+    this.meta.updateTag({ name: 'keywords', content: 'Order Birthday Cake Online ,order cake online ,birthday cakes delivered ,send cake to India ,online cake delivery in india ,send birthday cake online' });
+    this.meta.updateTag({ name: 'classification', content: 'Order Birthday Cake Online ,order cake online ,birthday cakes delivered ,send cake to India ,online cake delivery in india ,send birthday cake online' });
 
     this.addLoader();
     this.addCanonicalLink();
 
-//localStorage.setItem('currency', 'INR')
-this.city=localStorage.getItem('city')
-this.countryname=localStorage.getItem('country');
-this.currency=localStorage.getItem('currency');
+    //localStorage.setItem('currency', 'INR')
+    this.city = localStorage.getItem('city')
+    this.countryname = localStorage.getItem('country');
+    this.currency = localStorage.getItem('currency');
     this.getBanners()
-if(this.city)
-{
-    this.getProducts();
-}
-}
+    if (this.city) {
+      this.getProducts();
+    }
+  }
 
   getBanners(): void {
     this.addLoader();
     this._crud.getBanners().subscribe(res => {
-     this.removeLoader();
-     this.banners=res.banners;
-    this.iconicBanners=res.iconicBanners;
-    this.smallBanners=res.smallBanners;
-    }, (error)=>{
+      this.removeLoader();
+      this.banners = res.banners;
+      this.iconicBanners = res.categoryBanners;
+      this.smallBanners = res.smallBanners;
+      this.promotionBanner = res.promotionBanner;
+      this.shopByGender = res.shopByGender;
+    }, (error) => {
       this.removeLoader()
-        })
+    })
   }
 
   private addCanonicalLink() {
@@ -86,81 +86,77 @@ if(this.city)
 
   getProducts(): void {
     this.addLoader();
-    const data={
-      cityname:this.city,
-      country:this.countryname,
-      currencySelected:this.currency
-    }    
+    const data = {
+      cityname: this.city,
+      country: this.countryname,
+      currencySelected: this.currency
+    }
     this._crud.getProducts(data).subscribe(res => {
-     this.removeLoader();
-     this.productSecions=res
-    }, (error)=>{
+      this.removeLoader();
+      this.productSecions = res
+    }, (error) => {
       this.removeLoader()
-        })
+    })
   }
 
-gotoroute( t:any, pname:any)
-{
+  gotoroute(t: any, pname: any) {
 
-let c=localStorage.getItem('city')?.toLowerCase();   
+    let c = localStorage.getItem('city')?.toLowerCase();
 
-  if(t == 'C')
-  {
-    t='online-delivery';
-    // this.route.navigateByUrl('/'+ t + '/'+ c + '/' + pname)
-    this.route.navigateByUrl('/'+ pname + '/'+ c + '/' + t) 
-  } else if(t=='SC')
-  {
-    t='order';
-    this.route.navigateByUrl('/'+ t + '/'+ c + '/' + pname)
-  }
-  else if(t=='OCC')
-  {
-    t='send';
-    this.route.navigateByUrl('/'+ t + '/'+ c + '/' + pname)
-  }
-  else if(t=='SPL')
-  {
-  
-    this.route.navigateByUrl('/'+ pname +'-' + c)
-  }
-  else if(t=='FLV')
-  {
-    
-    let link=pname + '-to-' +c
-    this.route.navigateByUrl('/'+ link)
-  }
-  else if(t=='CTY')
-  {
-    
-   
-    this.route.navigateByUrl('/'+c +'/'+pname)
-  }
- 
-  else
-  {
-    // t=t
-  }
+    if (t == 'C') {
+      t = 'online-delivery';
+      // this.route.navigateByUrl('/'+ t + '/'+ c + '/' + pname)
+      this.route.navigateByUrl('/' + pname + '/' + c + '/' + t)
+    } else if (t == 'SC') {
+      t = 'order';
+      this.route.navigateByUrl('/' + t + '/' + c + '/' + pname)
+    }
+    else if (t == 'OCC') {
+      t = 'send';
+      this.route.navigateByUrl('/' + t + '/' + c + '/' + pname)
+    }
+    else if (t == 'SPL') {
+
+      this.route.navigateByUrl('/' + pname + '-' + c)
+    }
+    else if (t == 'FLV') {
+
+      let link = pname + '-to-' + c
+      this.route.navigateByUrl('/' + link)
+    }
+    else if (t == 'CTY') {
 
 
-}
+      this.route.navigateByUrl('/' + c + '/' + pname)
+    }
 
-advbanner01:string="../assets/images/advbanner01.jpg";
-advbanner02:string="../assets/images/advbanner02.jpg";
-videoAdv:string="../assets/images/video-adv.jpg";
-videoImgTwo:string="../assets/images/video-img-two.jpg";
-womenJewelleryImg:string="../assets/images/women-jewellery-img.jpg";
-menJewelleryImg:string="../assets/images/men-jewellery-img.jpg";
-fullBannerImg:string="../assets/images/fullbanner-footer.jpg";
+    else {
+      // t=t
+    }
 
-homeVideos=[
-  {imageUrl: '../assets/images/videos-images/video01.jpg'},  
-  {imageUrl: '../assets/images/videos-images/video02.jpg'},  
-  {imageUrl: '../assets/images/videos-images/video03.jpg'},  
-  {imageUrl: '../assets/images/videos-images/video04.jpg'},  
-  {imageUrl: '../assets/images/videos-images/video05.jpg'},  
-  {imageUrl: '../assets/images/videos-images/video01.jpg'},  
-];
+
+  }
+  onImageError(event: Event) {
+    const imgElement = event.target as HTMLImageElement;
+    imgElement.src = this.defaultImageUrl;
+  }
+  defaultImageUrl: string = "/assets/images/default.jpeg";
+  advbanner01: string = "../assets/images/advbanner01.jpg";
+  advbanner02: string = "../assets/images/advbanner02.jpg";
+  videoAdv: string = "../assets/images/video-adv.jpg";
+  videoImgTwo: string = "../assets/images/video-img-two.jpg";
+  womenJewelleryImg: string = "../assets/images/women-jewellery-img.jpg";
+  menJewelleryImg: string = "../assets/images/men-jewellery-img.jpg";
+  fullBannerImg: string = "../assets/images/fullbanner-footer.jpg";
+
+  homeVideos = [
+    { imageUrl: '../assets/images/videos-images/video01.jpg' },
+    { imageUrl: '../assets/images/videos-images/video02.jpg' },
+    { imageUrl: '../assets/images/videos-images/video03.jpg' },
+    { imageUrl: '../assets/images/videos-images/video04.jpg' },
+    { imageUrl: '../assets/images/videos-images/video05.jpg' },
+    { imageUrl: '../assets/images/videos-images/video01.jpg' },
+  ];
 
 
 
