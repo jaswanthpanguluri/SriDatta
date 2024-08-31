8fcaa37e6006475610307d713270491283f688d6
Reddy <laxma.pltech@gmail.com>
2024-08-31 18:09:09 +0530

    changes reverted

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -258,15 +258,17 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
 
         if (this.typeName == 'online-delivery' || this.PageName == 'online-delivery') {
-          this.type = 'C';
+          //this.type = 'C';
           // this.PageName=this.PageName;
+          this.type = 'online-delivery';
 
 
           this.PageName = params['type'];
 
-        } else if (this.typeName == 'order') {
-          this.type = 'SC';
-
+        }
+        else if (this.typeName == 'order') {
+          //this.type = 'SC';
+          this.type = 'order';
         }
         else if (this.typeName == 'send') {
           this.type = 'OCC';
